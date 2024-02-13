52a5c3eaa1e45c805c9d760b16c7403081448f51
sanghoon kang <sanghoon752@gmail.com>
2024-02-13 18:44:22 +0900

    Exercise 2-7B Completed

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -1,7 +1,7 @@
 import { LightningElement, wire } from 'lwc';
 
 // TODO #1: import the getRecord, getFieldValue, and getFieldDisplayValue functions from lightning/uiRecordApi.
-import{getRecord, getFieldValue, getFieldDisplayValue} from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
 // TODO #2: We've imported the name field and placed it into an array for you.
 //          To prepare for Lab 1, import the Description, Email, and Phone fields and add them to the array.
 import FIELD_Name from '@salesforce/schema/Contact.Name';
@@ -14,28 +14,27 @@ export default class StudentDetail extends LightningElement {
 
 	// TODO #3: locate a valid Contact ID in your scratch org and store it in the studentId property.
 	// Example: studentId = '003S000001SBAXEIA5';
-	studentId = '0031K00003LKhsBQAT';
+	studentId = '0035200000tzivrAAA';
 
 	//TODO #4: use wire service to call getRecord, passing in our studentId and array of fields.
 	//		   Store the result in a property named wiredStudent.
-	@wire(getRecord, {recordId: '$studentId', fields})
-	wiredStudent;
+	@wire(getRecord, { recordId: '$studentId', fields })wiredStudent;
 		
 	get name() {
 		return this._getDisplayValue(this.wiredStudent.data, FIELD_Name);
 	}
 
-    get description(){
-        return this._getDisplayValue(this.wiredStudent.data, FIELD_Description);
-    }
-    
-    get phone(){
-        return this._getDisplayValue(this.wiredStudent.data, FIELD_Phone);
-    }
+    get description() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Description);
+	}
 
-    get email(){
-        return this._getDisplayValue(this.wiredStudent.data, FIELD_Email);
-    }
+	get phone() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Phone);
+	}
+
+	get email() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Email);
+	}
 
 	//TODO #5: We provided a getter for the name field. 
 	// 		   To prepare for Lab 1, create getters for the description, phone, and email fields.
